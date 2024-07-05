172006f079a10e7de4b635cebf4f7b935f3cbdc5
Fozilkhon <buzrukxojayevfozilxon2007@gmail.com>
2024-07-05 17:14:13 +0500

    done

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -5,15 +5,17 @@ import Team from '../../components/Team';
 import FAQ from '../../components/FAQ';
 import Contact from '../../components/Contact';
 
+import './style.css';
+
 const Home = () => {
   return (
-    <>
+    <div className='home-container'>
       <Opener />
       <Posts />
       <Contact />
       <Team />
       <FAQ />
-    </>
+    </div>
   );
 };
 
